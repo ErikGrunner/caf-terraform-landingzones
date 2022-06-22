01a9b0a2b0f7944d9b81c5597f30c3ce485686a1
Erik Grunner <talk4erik@gmail.com>
2022-06-22 15:31:00 +0000

    Update definition files.

--- templates/platform/deploy_platform.sh
+++ templates/platform/deploy_platform.sh
@@ -5,7 +5,7 @@ export ANSIBLE_DISPLAY_SKIPPED_HOSTS=False
 
 params=$(echo ${@} | xargs -n1 | xargs -I@ echo "-e @ " )
 
-ansible-playbook /tf/caf/landingzones/templates/ansible/walk-through-single.yaml \
+ansible-playbook caf/templates/ansible/walk-through-single.yaml \
   -e topology_file=/tf/caf/landingzones/templates/platform/single_subscription.yaml \
   -e public_templates_folder=/tf/caf/landingzones/templates \
   -e landingzones_folder=/tf/caf/landingzones \
@@ -14,7 +14,7 @@ ansible-playbook /tf/caf/landingzones/templates/ansible/walk-through-single.yaml
   -e platform_template_folder=/tf/caf/platform/template \
   -e caf_landingzone_branch='2204.1.int' \
   --extra-vars "@/tf/caf/landingzones/templates/platform/template_topology.yaml" \
-  -e $(echo ${params} | xargs)
+  #-e $(echo ${params} | xargs)
 
 # Create the initial PR for the bootstrap configuration
 body=<<EOF
